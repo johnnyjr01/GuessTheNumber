38552c99246d9da65d21c32ce135be1711df08cb
gvasapollo1@gmail.com <gvasapollo1@gmail.com>
2025-08-08 17:42:13 +0200

    Difficulty levels

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,14 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to the Number Guessing Game!\n" +
-                "I'm thinking of a number between 1 and 100.\n" + "Please select the difficulty level:");
-        System.out.println("");
+                "I'm thinking of a number between 1 and 100.\n");
+        System.out.println("Please select the difficulty level");
+        System.out.println("1 - Easy (20 tries");
+        System.out.println("2 - Medium (15 tries)");
+        System.out.println("3 - Hard (10 tries)");
+        System.out.println("4 - Super Hard (5 tries)");
+
+
 
 
 
