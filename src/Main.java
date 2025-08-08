cac919c49d164198971c2f33437b7cfe2057981c
gvasapollo1@gmail.com <gvasapollo1@gmail.com>
2025-08-08 17:31:36 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Number Guessing Game!\n" +
+                "I'm thinking of a number between 1 and 100.\n" + "Please select the difficulty level:");
+        System.out.println("");
+
+
+
+
+
+
+    }
+}
